b2dc6e8d6644564b14e1288f88fe14ba0d2e18ee
itsshortyYS <itsshortyys@gmail.com>
2026-02-28 15:13:18 -0600

    added smithing recipes for netherite hammers

--- kubejs/server_scripts/recipes/add_misc_recipes.js
+++ kubejs/server_scripts/recipes/add_misc_recipes.js
@@ -19,4 +19,36 @@ ServerEvents.recipes((event) => {
         "minecraft:fishing_rod",
         "starcatcher:bobber"
     )
+
+    //Just Hammers
+    event.smithing(
+        "justhammers:netherite_hammer",
+        "minecraft:netherite_upgrade_smithing_template",
+        "justhammers:diamond_hammer",
+        "minecraft:netherite_ingot"
+    )
+    event.smithing(
+        "justhammers:netherite_impact_hammer",
+        "justhammers:impact_core",
+        "justhammers:diamond_impact_hammer",
+        "minecraft:netherite_block"
+    )
+    event.smithing(
+        "justhammers:netherite_reinforced_hammer",
+        "justhammers:reinforced_core",
+        "justhammers:diamond_reinforced_hammer",
+        "minecraft:netherite_block"
+    )
+    event.smithing(
+        "justhammers:netherite_reinforced_impact_hammer",
+        "justhammers:reinforced_impact_core",
+        "justhammers:diamond_reinforced_impact_hammer",
+        "minecraft:netherite_block"
+    )
+    event.smithing(
+        "justhammers:netherite_destructor_hammer",
+        "justhammers:destructor_core",
+        "justhammers:diamond_destructor_hammer",
+        "minecraft:netherite_block"
+    )
 })
