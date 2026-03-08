78ea736bec6e89867edc05c6e2f602b849ef7052
itsshortyYS <itsshortyys@gmail.com>
2026-03-08 00:54:05 -0600

    reformatted ball recipes

--- kubejs/server_scripts/recipes/add_ball_recipes.js
+++ kubejs/server_scripts/recipes/add_ball_recipes.js
@@ -1,27 +1,56 @@
 console.info("[BAB] AddCobblemonBallRecipes.js loaded")
 
 ServerEvents.recipes(event => {
-    const stampedLidRecipes = [
-        // [output, targetItem, count]
-        ["createmonballsoverhaul:brass_lid", {tag: "c:nuggets/brass"}, 1],
-        ["createmonballsoverhaul:copper_lid", {tag: "c:nuggets/copper"}, 1],
-        ["createmonballsoverhaul:iron_lid", {tag: "c:nuggets/iron"}, 1],
-        ["createmonballsoverhaul:sturdy_lid", {item: "create:sturdy_sheet"}, 1],
-        ["createmonballsoverhaul:apricorn_lid", {item: "cobblemon:stripped_apricorn_log"}, 1]
-    ]
-    
-    stampedLidRecipes.forEach(([output, targetItem, count]) => {
+    const deployingRecipe = (output, targetItem, deployedItem, keepHeld) => {
         event.custom({
             type: "create:deploying",
             ingredients: [
                 targetItem,
-                {item: "createmonballsoverhaul:rubber_stamp_lid"}
+                {item: deployedItem}
             ],
-            result: [{
-                item: output,
-                count: count
+            results: [{
+                id: output
             }],
-            keep_held_item: true
+            keep_held_item: keepHeld
         })
+    }
+    
+    const recipesStampedLid = [
+        {
+            output: "createmonballsoverhaul:stamped_brass_nugget_lid",
+            targetItem: {tag: "c:nuggets/brass"},
+            deployedItem: "createmonballsoverhaul:rubber_stamp_lid",
+            keepHeld: true
+        },
+        {
+            output: "createmonballsoverhaul:stamped_copper_nugget_lid",
+            targetItem: {tag: "c:nuggets/copper"},
+            deployedItem: "createmonballsoverhaul:rubber_stamp_lid",
+            keepHeld: true
+        },
+        {
+            output: "createmonballsoverhaul:stamped_iron_nugget_lid",
+            targetItem: {tag: "c:nuggets/iron"},
+            deployedItem: "createmonballsoverhaul:rubber_stamp_lid",
+            keepHeld: true
+        },
+        {
+            output: "createmonballsoverhaul:stamped_sturdy_sheet_lid",
+            targetItem: {item: "create:sturdy_sheet"},
+            deployedItem: "createmonballsoverhaul:rubber_stamp_lid",
+            keepHeld: true
+        },
+        {
+            output: "createmonballsoverhaul:stamped_stripped_apricorn_log_lid",
+            targetItem: {item: "cobblemon:stripped_apricorn_log"},
+            deployedItem: "createmonballsoverhaul:rubber_stamp_lid",
+            keepHeld: true
+        }
+    ]
+    
+    recipesStampedLid.forEach(recipe => { 
+        deployingRecipe(
+            recipe.output, recipe.targetItem, recipe.deployedItem, recipe.keepHeld
+        )
     })
 })
